8c4e234d0d24132dcb6c08b101dfbdb23d6c7132
cristian-hanca <cristian.hanca@gmail.com>
2016-11-12 10:10:55 +0100

    Added basic Map.

---
+++ app/src/main/java/ro/hanca/cristian/munichcity/control/MapControl.java
@@ -0,0 +1,67 @@
+package ro.hanca.cristian.munichcity.control;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import ro.hanca.cristian.munichcity.AppContext;
+import ro.hanca.cristian.munichcity.R;
+
+/**
+ * Custom UI Control to display Points on a map.
+ */
+public class MapControl extends LinearLayout implements OnMapReadyCallback {
+
+    private GoogleMap map;
+
+    public MapControl(Context context) {
+        super(context);
+        init();
+    }
+
+    public MapControl(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MapControl(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        LayoutInflater inflater = (LayoutInflater)
+                AppContext.activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View v = inflater.inflate(R.layout.control_map, this);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) AppContext.activity
+                .getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        updateUI();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng munich = new LatLng(48.1351, 11.5820);
+        map.addMarker(new MarkerOptions().position(munich).title("Marker in Munich"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(munich));
+    }
+
+    private void updateUI() {
+
+    }
+}
